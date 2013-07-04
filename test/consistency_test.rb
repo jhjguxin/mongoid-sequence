8d433564fe7840172cabad362df33439acffdab4
francis jiang <864248765@qq.com>
2013-07-04 11:04:51 +0800

    add case 'test_concurrent_sequence'

--- test/consistency_test.rb
+++ test/consistency_test.rb
@@ -1,12 +1,29 @@
 require "test_helper"
 
-class SequenceTest < BaseTest
+class ConsistencyTest < BaseTest
   def test_single_sequence_consistency
     n = 200
 
     n.times do
       FirstSequencedModel.create
     end
+
+    assert_equal FirstSequencedModel.only(:auto_increment).map(&:auto_increment).sort, (1..n).to_a
+  end
+
+  def test_concurrent_sequence
+    n = 500
+
+    thread_group = ThreadGroup.new
+
+    n.times {
+      thread =Thread.new {
+          FirstSequencedModel.create
+        }
+      thread_group.add thread
+    }
+
+    thread_group.list.collect{|thread| thread.join}
 
     assert_equal FirstSequencedModel.only(:auto_increment).map(&:auto_increment).sort, (1..n).to_a
   end
